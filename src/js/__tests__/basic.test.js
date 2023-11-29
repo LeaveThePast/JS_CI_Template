9f0b3dfcdbb47c3fa1f77cc7c23726cadb894af4
LeaveThePast <Ramil.Budagli@yandex.ru>
2023-11-29 19:49:45 +0400

    Updated

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,74 +1,103 @@
-import { assessHealth, sortCharactersByHealth, getLevel } from '../basic';
-import fetchData from '../http';
-//Base
-test.each([
-  [{ name: "Warrior", health: 90 }, "healthy"],
-  [{ name: "Rogue", health: 69 }, "wounded"],
-  [{ name: "Mage", health: 10 }, "critical"]
-])("character is %s and health is %i", (character, expected) => {
-  expect(assessHealth(character)).toBe(expected);
-});
+import * as Characters from "../basic";
 
-test("sort heroes by health", () => {
-  const characters = [
-    { name: "Warrior", health: 10 },
-    { name: "Mage", health: 100 },
-    { name: "Rogue", health: 80 }
-  ];
-  const sortedCharacters = sortCharactersByHealth(characters)
-
-  expect(sortedCharacters).toEqual([
-    { name: "Mage", health: 100 },
-    { name: "Rogue", health: 80 },
-    { name: "Warrior", health: 10 }
-  ]);
-});
-
-//Requests
-jest.mock('../http');
-
-describe('getLevel function', () => {
-  it('should return the current level when response status is "ok"', () => {
-    const mockResponse = {
-      status: 'ok',
-      level: 42,
-    };
-
-    fetchData.mockReturnValueOnce(mockResponse);
-
-    const result = getLevel('123');
-
-    expect(result).toBe('Ваш текущий уровень: 42');
+describe("Character", () => {
+  it("should throw an error if name is too short", () => {
+    expect(() => new Characters.Character("a", "Bowman")).toThrow("Имя должно содержать от двух до десяти символов");
   });
 
-  it('should return an error message when response status is not "ok"', () => {
-    const mockResponse = {
-      status: 'error',
-    };
+  it("should throw an error if name is too long", () => {
+    expect(() => new Characters.Character("aaaaaaaaaaaa", "Bowman")).toThrow("Имя должно содержать от двух до десяти символов");
+  });
 
-    fetchData.mockReturnValueOnce(mockResponse);
+  it("shoud throw an error if type is not exists", () => {
+    expect(() => new Characters.Character("ab", "Rogue")).toThrow("Некорректный тип персонажа");
+  });
 
-    const result = getLevel('456');
-
-    expect(result).toBe('Информация об уровне временно недоступна');
+  it("shoud create a character with correct properties", () => {
+    const character = new Characters.Character("abc", "Bowman");
+    expect(character.name).toEqual("abc");
+    expect(character.type).toEqual("Bowman");
+    expect(character.health).toEqual(100);
+    expect(character.level).toEqual(1);
   });
 });
 
-describe('getLevel function error handling', () => {
-  it('should handle errors thrown by fetchData', () => {
-    fetchData.mockImplementationOnce(() => {
-      throw new Error('Mocked fetchData error');
-    });
+describe("Character methods", () => {
+  it("levelUp shoud throw an error for a dead character", () => {
+    const character = new Characters.Bowman("abc");
+    character.health = 0;
+    expect(() => character.levelUp()).toThrow("Нельзя повысить уровень умершего персонажа");
+  });
 
-    let result;
-    try {
-      result = getLevel('789');
-    } catch (error) {
-      console.error('An error occurred:', error.message);
-    }
+  it("levelUp shoud be correct", () => {
+    const character = new Characters.Bowman("abc");
+    character.health = 80;
+    character.levelUp();
+    expect(character.level).toEqual(2);
+    expect(character.attack).toEqual(48);
+    expect(character.defence).toEqual(12);
+    expect(character.health).toEqual(100);
+  })
 
-    result = result || 'Информация об уровне временно недоступна';
+  it("damage shoud throw an error for dead character", () => {
+    const character = new Characters.Bowman("abc");
+    character.health = 0;
+    expect(() => character.damage(10)).toThrow("Нельзя нанести урон умершему персонажу");
+  });
 
-    expect(result).toBe('Информация об уровне временно недоступна');
+  it("damage shoud reduce health based on points and defence", () => {
+    const character = new Characters.Bowman("abc");
+    character.damage(10);
+    expect(character.health).toEqual(91);
+  });
+});
+
+describe('Character classes', () => {
+  it('Bowman should have correct properties', () => {
+    const bowman = new Characters.Bowman('Bowman1');
+    expect(bowman.name).toBe('Bowman1');
+    expect(bowman.type).toBe('Bowman');
+    expect(bowman.attack).toBe(40);
+    expect(bowman.defence).toBe(10);
+  });
+
+  it('Swordsman should have correct properties', () => {
+    const swordsman = new Characters.Swordsman('Swordsman1');
+    expect(swordsman.name).toBe('Swordsman1');
+    expect(swordsman.type).toBe('Swordsman');
+    expect(swordsman.attack).toBe(40);
+    expect(swordsman.defence).toBe(10);
+  });
+
+  test('Magician should have correct properties', () => {
+    const magician = new Characters.Magician('Magician1');
+    expect(magician.name).toBe('Magician1');
+    expect(magician.type).toBe('Magician');
+    expect(magician.attack).toBe(10);
+    expect(magician.defence).toBe(40);
+  });
+
+  it('Undead should have correct properties', () => {
+    const undead = new Characters.Undead('Undead1');
+    expect(undead.name).toBe('Undead1');
+    expect(undead.type).toBe('Undead');
+    expect(undead.attack).toBe(25);
+    expect(undead.defence).toBe(25);
+  });
+
+  it('Zombie should have correct properties', () => {
+    const zombie = new Characters.Zombie('Zombie1');
+    expect(zombie.name).toBe('Zombie1');
+    expect(zombie.type).toBe('Zombie');
+    expect(zombie.attack).toBe(40);
+    expect(zombie.defence).toBe(10);
+  });
+
+  it('Daemon should have correct properties', () => {
+    const daemon = new Characters.Daemon('Daemon1');
+    expect(daemon.name).toBe('Daemon1');
+    expect(daemon.type).toBe('Daemon');
+    expect(daemon.attack).toBe(10);
+    expect(daemon.defence).toBe(40);
   });
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,28 +1,82 @@
-function assessHealth(character) {
-  const health = character.health;
-
-  if (health >= 70) {
-    return "healthy";
+class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error("Имя должно содержать от двух до десяти символов")
+    }
+    const types = ["Bowman", "Swordsman", "Magician", "Undead", "Zombie", "Daemon"]
+    if (!types.includes(type)) {
+      throw new Error("Некорректный тип персонажа")
+    }
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
   }
-  if (health >= 30) {
-    return "wounded";
-  } else {
-    return "critical";
+
+  levelUp() {
+    if (this.health <= 0) {
+      throw new Error("Нельзя повысить уровень умершего персонажа")
+    }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health <= 0) {
+      throw new Error("Нельзя нанести урон умершему персонажу")
+    }
+    this.health -= points * (1 - this.defence / 100)
   }
 }
 
-function sortCharactersByHealth(characters) {
-  return characters.sort((a, b) => b.health - a.health);
-}
-
-import fetchData from './http';
-
-function getLevel(userId) {
-  const response = fetchData(`https://server/user/${userId}`);
-  if (response.status === 'ok') {
-    return `Ваш текущий уровень: ${response.level}`;
+class Bowman extends Character {
+  constructor(name) {
+    super(name, "Bowman");
+    this.attack = 40;
+    this.defence = 10;
   }
-  return `Информация об уровне временно недоступна`;
 }
 
-export { assessHealth, sortCharactersByHealth, getLevel };
+class Swordsman extends Character {
+  constructor(name) {
+    super(name, "Swordsman");
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+class Magician extends Character {
+  constructor(name) {
+    super(name, "Magician");
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+class Undead extends Character {
+  constructor(name) {
+    super(name, "Undead");
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+class Zombie extends Character {
+  constructor(name) {
+    super(name, "Zombie");
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+class Daemon extends Character {
+  constructor(name) {
+    super(name, "Daemon");
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export { Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon };
