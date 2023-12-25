5815fc786548822159eb18486c5e392485eb0792
LeaveThePast <105272588+LeaveThePast@users.noreply.github.com>
2023-12-25 23:32:20 +0400

    Update saving.test.js

--- src/js/__tests__/saving.test.js
+++ src/js/__tests__/saving.test.js
@@ -1,21 +1,9 @@
-import { GameSaving, GameSavingLoader } from "../saving";
+import { GameSavingLoader } from "../saving";
 
 describe('GameSavingLoader', () => {
-  it("should create correct saving GameSaving", () => {
-    const data = {
-      id: 9,
-      created: 1546300800,
-      userInfo: {
-        id: 1,
-        name: 'Hitman',
-        level: 10,
-        points: 2000,
-      }
-    }
-    expect(new GameSaving(data)).toEqual(data)
-  })
+  const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
   it('should load game saving data', async () => {
-    const saving = await GameSavingLoader.load();
+    const saving = await GameSavingLoader.load(data);
     expect(saving).toEqual({
       id: 9,
       created: 1546300800,
